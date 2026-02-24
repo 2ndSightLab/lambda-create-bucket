fec4a2ea198634119f0e136d3236d327f13435c8
2ndsightlab <idontwantosetanemail@github.com>
2026-02-24 18:31:01 +0000

    1

--- src/init.sh
+++ src/init.sh
@@ -5,4 +5,4 @@ BUCKET=$LAMBDA
 CLOUDWATCH_LOG_GROUP=$LAMBDA
 LAMBDA_ROLE=$LAMBDA-lambda-role
 LAMBDA_ROLE_POLICY=$LAMBDA-lambda-policy
-REGION=$(aws configure get region)
+REGION=${AWS_REGION:-${AWS_DEFAULT_REGION:-$(aws configure get region)}}
